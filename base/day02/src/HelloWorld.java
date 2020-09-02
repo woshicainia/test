924a75e0c95fa23cdccfbf4e7eac67657e9d1090
wxwwang <1845913852@qq.com>
2020-09-02 10:56:19 +0800

    第二天写的代码

---
+++ base/day02/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Created by wangxiaowei on 2020/9/2.
+ * 我的第一个程序
+ */
+public class HelloWorld {
+    /*
+     类体：
+     类体中不能直接编写Java语句，除声明变量以外
+     变量：在类体内定义的变量叫成员变量
+    */
+    public static void main(String[] args) {
+        /*
+        方法体：
+        在方法体中的代码是自上问下依次执行的
+        在方法体中定义的变量叫局部变量
+         */
+        System.out.println("HelloWorld");//HelloWorld
+    }
+}
