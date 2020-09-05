a75adf43169571ddede615759c659951162c3303
wxwwang <1845913852@qq.com>
2020-09-05 09:23:50 +0800

    第五天的代码

---
+++ base/day05/src/Calculator.java
@@ -0,0 +1,16 @@
+/**
+ * Created by wangxiaowei on 2020/9/5.
+ * 定义一个两个整数相加的方法和一个两个整数相减的方法
+ * 用junit单元测试
+ */
+public class Calculator {
+    //定义一个两个整数相加的方法
+    public int add(int a,int b){
+        return a + b;
+    }
+
+    //两个整数相减的方法
+    public int sub(int a,int b){
+        return a - b;
+    }
+}
